5b0dbadedae19c2bd8da8f3592ca4a709df375de
Trey Evans <lewis.r.evans@gmail.com>
2015-06-24 17:53:54 -0400

    Now implements encrypt and compare.

--- lib/rypt/sha512.rb
+++ lib/rypt/sha512.rb
@@ -1,5 +1,6 @@
 require 'digest'
 require 'base64'
+require 'securerandom'
 
 module Rypt
   # Implements the standard crypt(3) call with SHA-512 and a salt.
@@ -9,6 +10,21 @@ module Rypt
   #   3. Finalize the hash result by reordering the bytes and base-64 encoding them using a custom mapping scheme
   # At the end we are going to take the result of #3 and spit it out concatenated with the seed as the final result.
   class Sha512
+    def self.encrypt(plain)
+      self.new.encrypt(plain)
+    end
+
+    def self.compare(encrypted, plain)
+      encryptor = self.new
+      salt = encryptor.extract_salt(encrypted)
+      return false unless salt
+      encrypted == self.new.run(salt, plain)
+    end
+
+    def extract_salt(encrypted)
+      encrypted.split("$")[2]
+    end
+
     def init_hash(salt, pass)
       alt_sum = Digest::SHA2.new(512).digest(pass + salt + pass)
       s_length = [salt.length, 16].min
@@ -75,5 +91,14 @@ module Rypt
 
       "$6$" + salt_trunc  + "$" + finalize_block(hash_val)
     end
+
+    def encrypt(plain)
+      run(generate_salt, plain)
+    end
+
+    def generate_salt
+      SecureRandom.hex(8)
+    end
+
   end
 end
--- spec/lib/rypt/sha512_spec.rb
+++ spec/lib/rypt/sha512_spec.rb
@@ -20,3 +20,52 @@ describe Rypt::Sha512 do
     expect(subject.run("salted", "password")).to eq "$6$salted$/zTQ7CjZH8wweVbQm04I7lDdky41N0fNJcioYrtxgIiY1qrrWcbdx8FcueRl/oJVk0UGKVeABtVvQBSbSpS6v0"
   end
 end
+
+shared_examples "matches encrypted sha512" do |encrypted_pass, pass|
+  it "should equate \"#{encrypted_pass}\" with \"#{pass}\"" do
+    expect(Rypt::Sha512.compare(encrypted_pass, pass)).to eq true
+  end
+end
+
+describe Rypt::Sha512, "given a pre-encrypted value to compare with a password" do
+  include_examples "matches encrypted sha512", "$6$salted$/zTQ7CjZH8wweVbQm04I7lDdky41N0fNJcioYrtxgIiY1qrrWcbdx8FcueRl/oJVk0UGKVeABtVvQBSbSpS6v0", "password"
+end
+
+describe Rypt::Sha512, "asked to generate a salt" do
+  let(:secure_random_salt) { "salt from securerandom" }
+
+  it "should provide a salt 16 characters long" do
+    expect(subject.generate_salt.length).to eq 16
+  end
+
+  it "should obtain the salt from SecureRandom" do
+    allow(SecureRandom).to receive(:hex).with(8).and_return(secure_random_salt)
+    expect(subject.generate_salt).to eq secure_random_salt
+  end
+end
+
+describe Rypt::Sha512, "given only a plaintext value" do
+  let(:plaintext) { "some plain text" }
+
+  describe"should produce an encrypted value" do
+    let(:ciphertext) { Rypt::Sha512.encrypt(plaintext) }
+
+    specify "which is not nil" do
+      expect(ciphertext).not_to eq nil
+    end
+
+    specify "which is different from the plaintext" do
+      expect(ciphertext).not_to eq plaintext
+    end
+
+    specify "which contains a 16-char salt" do
+      salt = ciphertext.split("$")[2]
+      expect(salt.length).to eq 16
+    end
+
+    specify "which contains the encrypted hash" do
+      hash = ciphertext.split("$")[3]
+      expect(hash.length).to eq 86
+    end
+  end
+end
